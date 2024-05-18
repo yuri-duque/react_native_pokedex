b8ba936eb99c59fd5601f5bdb4603361f50f984f
yuri-duque <yurithielmann83@gmail.com>
2024-05-18 17:47:16 -0300

    configuring environment variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
+import {POKEDEX_API_URL} from '@env';
 import React from 'react';
 import {StatusBar} from './components/StatusBar';
 import {AppStack} from './routes';
 
 export const App = () => {
+  console.log('api', POKEDEX_API_URL);
+
   return (
     <>
       <StatusBar backgroundColor="#5E8D48" barStyle="light-content" />
---
+++ src/types/env.d.ts
@@ -0,0 +1,3 @@
+declare module '@env' {
+  export const POKEDEX_API_URL: string;
+}
