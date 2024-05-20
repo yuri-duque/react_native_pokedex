97585efe2017f17db03b4f01769fcc1bd949c077
yuri-duque <yurithielmann83@gmail.com>
2024-05-20 16:12:53 -0300

    consumingo pokemon api

--- src/screens/details/index.tsx
+++ src/screens/details/index.tsx
@@ -1,10 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import { pokemonById } from '../../service/pokemonApi';
+import { Pokemon } from '../../models/pokemon';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 export const DetailsScreen = () => {
+  const [pokemon, setPokemon] = useState<Pokemon>();
+
+  const getPokemon = async () => {
+    const pokemon = await pokemonById(1);
+    setPokemon(pokemon);
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>Details Screen</Text>
+      <Icon name="arrowleft" size={30} color="#900" />
+      <Text>Pokemon:</Text>
+      {pokemon && <Text>{pokemon.name}</Text>}
     </SafeAreaView>
   );
 };
--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
