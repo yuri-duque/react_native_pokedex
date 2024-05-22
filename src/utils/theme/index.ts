be3ea3178a5b654544f63a83308045159b2f6320
yuri-duque <yurithielmann83@gmail.com>
2024-05-21 21:15:37 -0300

    updating theme colors

--- src/utils/theme/index.ts
+++ src/utils/theme/index.ts
@@ -1,6 +1,7 @@
 type TypeColor = {
   main: string;
   light: string;
+  dark: string;
 };
 
 export type Theme = {
@@ -30,7 +31,7 @@ export type Theme = {
       water: TypeColor;
     };
   };
-}
+};
 
 export const DefaultTheme: Theme = {
   colors: {
@@ -39,24 +40,24 @@ export const DefaultTheme: Theme = {
     success: '#28a745',
     error: '#dc3545',
     pokemonTypeColor: {
-      bug: {main: '#A6B91A', light: ''},
-      dark: {main: '#705746', light: ''},
-      dragon: {main: '#6F35FC', light: ''},
-      electric: {main: '#F7D02C', light: ''},
-      fairy: {main: '#D685AD', light: ''},
-      fighting: {main: '#C22E28', light: ''},
-      fire: {main: '#EE8130', light: ''},
-      flying: {main: '#A98FF3', light: ''},
-      ghost: {main: '#735797', light: ''},
-      grass: {main: '#7AC74C', light: ''},
-      ground: {main: '#E2BF65', light: ''},
-      ice: {main: '#96D9D6', light: ''},
-      normal: {main: '#A8A77A', light: ''},
-      poison: {main: '#A33EA1', light: ''},
-      psychic: {main: '#F95587', light: ''},
-      rock: {main: '#B6A136', light: ''},
-      steel: {main: '#B7B7CE', light: ''},
-      water: {main: '#6390F0', light: ''},
+      fire: {main: '#fb6c6c', light: '#f88c8b', dark: '#c55656'},
+      grass: {main: '#48d0b0', light: '#5ddfc6', dark: '#3aa88b'},
+      water: {main: '#75bcfc', light: '#8fd1fe', dark: '#5a95c3'},
+      electric: {main: '#ffd86f', light: '#ffe580', dark: '#d1ab58'},
+      bug: {main: '#A6B91A', light: '#C0D74E', dark: '#879014'},
+      dark: {main: '#705746', light: '#8C7261', dark: '#5C4535'},
+      dragon: {main: '#6F35FC', light: '#8A5FFD', dark: '#592BC7'},
+      fairy: {main: '#D685AD', light: '#E7A1C0', dark: '#AD6A8C'},
+      fighting: {main: '#C22E28', light: '#D85250', dark: '#9E241F'},
+      flying: {main: '#A98FF3', light: '#C0A7F8', dark: '#8671C3'},
+      ghost: {main: '#735797', light: '#8E70AD', dark: '#5B466D'},
+      ground: {main: '#E2BF65', light: '#EED987', dark: '#B89A4F'},
+      ice: {main: '#96D9D6', light: '#B1E6E4', dark: '#75AAA7'},
+      normal: {main: '#A8A77A', light: '#C0C08C', dark: '#87875E'},
+      poison: {main: '#A33EA1', light: '#B95FB6', dark: '#812F7F'},
+      psychic: {main: '#F95587', light: '#FA789F', dark: '#C0446C'},
+      rock: {main: '#B6A136', light: '#D0BC5B', dark: '#8F812A'},
+      steel: {main: '#B7B7CE', light: '#CDCCD9', dark: '#8F8F9E'},
     },
   },
 };
