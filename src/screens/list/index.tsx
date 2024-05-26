15fa047a96b8726eb93e77fa5e7513f83b44117e
yuri-duque <yurithielmann83@gmail.com>
2024-05-26 01:07:22 -0300

    updating screen

--- src/screens/list/index.tsx
+++ src/screens/list/index.tsx
@@ -1,27 +1,40 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {usePokemonContext} from '../../contexts/Pokemon';
 import {MainView} from './styles';
-import { pokemonGetAll } from '../../service/pokemonApi';
 import { PokemonList } from '../../components/PokemonList';
+import { TextInput } from 'react-native';
+import { Pokemon } from '../../models/pokemon';
+import { pokemonApi } from '../../service/pokemonApi';
 
 export const ListScreen = () => {
   const {pokemons, setPokemons} = usePokemonContext();
+  const [search, setSearch] = useState('');
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>([]);
 
   const getPokemons = async () => {
-    const pokemons = await pokemonGetAll();
+    const pokemons = await pokemonApi.getAll();
     setPokemons(pokemons);
+    setFilteredPokemons(pokemons);
+  };
+
+  const onChangeText = (text: string) => {
+    setSearch(text);
+    const filteredPokemons = pokemons?.filter((pokemon) => {
+      return pokemon.name.includes(text.toLowerCase());
+    });
+    setFilteredPokemons(filteredPokemons ?? []);
   };
 
   useEffect(() => {
     getPokemons();
   }, []);
-
   
   return (
     <SafeAreaView>
       <MainView>
-        <PokemonList pokemons={pokemons} />
+        {/* <TextInput value={search} onChangeText={onChangeText}/> */}
+        <PokemonList pokemons={filteredPokemons} />
       </MainView>
     </SafeAreaView>
   );
--- src/screens/list/styles.ts
+++ src/screens/list/styles.ts
@@ -4,5 +4,4 @@ export const MainView = styled.View`
   width: 100%;
   height: 100%;
   background-color: ${({theme}) => theme.colors.white};
-  padding: 16px;
 `;
