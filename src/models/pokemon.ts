3346496a0b275a0e6b63e9676ccd68f1e10c1745
yuri-duque <yurithielmann83@gmail.com>
2024-05-26 01:06:29 -0300

    updating model

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,8 +1,31 @@
+type PokemonType = {
+  name:
+    | 'fire'
+    | 'grass'
+    | 'water'
+    | 'electric'
+    | 'bug'
+    | 'dark'
+    | 'dragon'
+    | 'fairy'
+    | 'fighting'
+    | 'flying'
+    | 'ghost'
+    | 'ground'
+    | 'ice'
+    | 'normal'
+    | 'poison'
+    | 'psychic'
+    | 'rock'
+    | 'steel';
+};
+
 export type Pokemon = {
-    id: number;
-    name: string;
-    sprites: {
-        front_default: string;
-        front_shiny: string;
-    };
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+    front_shiny: string;
+  };
+  types: PokemonType[];
 };
